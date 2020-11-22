84ce76aa68f0442db55b1237bd6e8321ac8feb93
yarnishdsf <38353841+yarnishdsf@users.noreply.github.com>
2020-11-22 21:05:03 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Layout } from 'antd';
 import './App.css';
+import CompAndProps from './components/CompAndProps';
+import Link from './elements/Link';
+import { Content, Header } from 'antd/lib/layout/layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout className="layout">
+        <Header style = {{ background: '#04304f ', padding : '30px', marginBottom:'35px'}}>
+        <div className="title" style={{float:"right", marginRight:'15px', fontSize:"1.5rem", color: "white"}}>KELOMPOK 44</div>
+        <div style={{textAlign: "left"}}>
+           <Link>Modul 2</Link>
+           <Link>Modul 3</Link>
+         </div>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="Components">
+         <CompAndProps bgcolor = "#dfe3e5"/>
+         </div></Content>
+        
+      </Layout>
     </div>
   );
 }
